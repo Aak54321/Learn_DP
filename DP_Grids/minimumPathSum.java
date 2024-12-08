2459381a052de414ad31a554dbb38815228386ff
Aakash <saakash2k4@gmail.com>
2024-12-08 22:20:27 +0530

    finale

--- DP_Grids/minimumPathSum.java
+++ DP_Grids/minimumPathSum.java
@@ -1,29 +1,38 @@
+import java.util.*;
+
 public class minimumPathSum {
     public static int minPathSum(int i, int j, int[][] grids) {
         // we've reached the proper end
-        if(i == 0 && j == 0)
+        if (i == 0 && j == 0)
             return grids[0][0];
         // no more path to go
-        if(i <0 && j < 0)
+        if (i < 0 && j < 0)
             return Integer.MAX_VALUE; // reason why i give this is it never gets select as we use min at the end
-        int up =  minPathSum(i-1,j,grids);
-        int left = minPathSum(i,j-1,grids);
-        return grids[i][j] + Math.min(up,left); // here
-   }
-   public static int minPathSumMemo(int i, int j, int[][] grids,int[][] dp) {
-        if(i == 0 && j == 0)
+        int up = (i > 0) ? minPathSum(i - 1, j, grids) : Integer.MAX_VALUE;
+        int left = (j > 0) ? minPathSum(i, j - 1, grids) : Integer.MAX_VALUE;
+        return grids[i][j] + Math.min(up, left); // here
+    }
+
+    public static int minPathSumMemo(int i, int j, int[][] grids, int[][] dp) {
+        if (i == 0 && j == 0)
             return grids[0][0];
-        if(i < 0 && j < 0)
+        if (i < 0 && j < 0)
             return Integer.MAX_VALUE;
-        if(dp[i][j] != -1)
+        if (dp[i][j] != -1)
             return dp[i][j];
-        int up = minPathSumMemo(i-1, j, grids,dp);
-        int left = minPathSumMemo(i, j-1, grids,dp);
-        return dp[i][j] = grids[i][j] + Math.min(up,left);
-   }
-   public static int minPathSumTab(int[][] grids) {
-      int n = grids.length;      // Number of rows
-        int m = grids[0].length;   // Number of columns
+        // to get neglected in the Math.min()
+        int up = Integer.MAX_VALUE;
+        int left = Integer.MAX_VALUE;
+        if (i > 0)
+            up = minPathSumMemo(i - 1, j, grids, dp);
+        if (j > 0)
+            left = minPathSumMemo(i, j - 1, grids, dp);
+        return dp[i][j] = grids[i][j] + Math.min(up, left);
+    }
+
+    public static int minPathSumTab(int[][] grids) {
+        int n = grids.length; // Number of rows
+        int m = grids[0].length; // Number of columns
 
         // DP table
         int[][] dp = new int[n][m];
@@ -47,6 +56,38 @@ public class minimumPathSum {
         }
 
         // The bottom-right cell contains the minimum sum path
-        return dp[n - 1][m - 1];  
-   }
+        return dp[n - 1][m - 1];
+    }
+
+    public static int minPathSumSpOpt(int[][] grids) {
+        int[] prev = new int[grids[0].length];
+
+        for (int i = 0; i < grids.length; i++) {
+            int[] temp = new int[prev.length];
+            for (int j = 0; j < grids[0].length; j++) {
+                if (i == 0 && j == 0)
+                    temp[j] = grids[i][j];
+                else {
+                    int up = (i > 0) ? prev[j] : Integer.MAX_VALUE;
+                    int left = (j > 0) ? temp[j - 1] : Integer.MAX_VALUE;
+                    temp[j] = grids[i][j] + Math.min(up, left);
+                }
+            }
+            prev = temp;
+        }
+        return prev[grids[0].length - 1];
+    }
+
+    public static void main(String[] args) {
+        int[][] arr = new int[][] {
+                { 5, 9, 6 },
+                { 11, 5, 2 }
+        };
+        int[][] dp = new int[arr.length][arr[0].length];
+        for (int[] d : dp)
+            Arrays.fill(d, -1);
+        int res = minPathSumMemo(arr.length - 1, arr[0].length - 1, arr, dp);
+        int res1 = minPathSum(arr.length-1, arr[0].length-1,arr);
+        System.out.println(res1);
+    }
 }
